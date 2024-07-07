48a7cc036708fdd390056a287b9f9253a6dd49d6
Muhammad Sunaam <msunaam02@gmail.com>
2024-07-08 03:53:50 +0500

    update

--- src/Components/PPTForm/pptForm.js
+++ src/Components/PPTForm/pptForm.js
@@ -1,7 +1,7 @@
 export default function PPTForm({ setProject, setShowCreate }) {
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 bg-[rgba(0,0,0,0.3)] flex items-center justify-center z-[200]">
-      <div className="bg-white p-8 gap-10 w-1/2 flex flex-col shadow-xl">
+      <div className="bg-white p-8 gap-10 md:w-1/2 flex flex-col shadow-xl">
         <h1 className="text-2xl text-center">
           Create AI Powered Presentations
         </h1>
@@ -14,9 +14,9 @@ export default function PPTForm({ setProject, setShowCreate }) {
             setProject(formData);
             setShowCreate(false);
           }}
-          className="grid grid-cols-5 gap-4"
+          className="grid grid-cols-1 grid-flow-row md:grid-cols-5 gap-4"
         >
-          <fieldset className="flex col-span-3 flex-col gap-1">
+          <fieldset className="flex md:col-span-3 flex-col gap-1">
             <label htmlFor="title" className="text-sm italic text-gray-500">
               Presentation Title
             </label>
@@ -28,7 +28,7 @@ export default function PPTForm({ setProject, setShowCreate }) {
               type="text"
             />
           </fieldset>
-          <fieldset className="flex flex-col col-span-1 gap-1">
+          <fieldset className="flex flex-col md:col-span-1 gap-1">
             <label htmlFor="numSlides" className="text-sm italic text-gray-500">
               Number of Slides
             </label>
@@ -42,7 +42,7 @@ export default function PPTForm({ setProject, setShowCreate }) {
               type="number"
             />
           </fieldset>
-          <fieldset className="flex flex-col col-span-5 gap-1">
+          <fieldset className="flex flex-col md:col-span-5 gap-1">
             <label
               htmlFor="additionalInstructions"
               className="text-sm italic text-gray-500"
